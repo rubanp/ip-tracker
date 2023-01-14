a96176b3fe9c26f70e72e795956af8da2a964e7c
Ruban Prakash <rubanprakash@hotmail.com>
2023-01-14 19:04:26 +0000

    create functions for parsing form data, getting info and passing to google maps function

--- components/IPSearch.vue
+++ components/IPSearch.vue
@@ -1,25 +1,43 @@
 <script setup lang="ts">
 const input = ref(null);
-function log(e) {
+const local = "http://localhost:9999/"
+const live = "https://ip-address-13e06d.netlify.app/"
+const emit = defineEmits(['location'])
+
+async function find(e) {
     const formData = new FormData(e.target);
     const data = [...formData.entries()];
     const ip = data[0][1];
-    console.log(ip);
+    const ipData = await fetch(`${local}.netlify/functions/getip?ip=${ip}`)
+    let receivedData = await ipData.json()
+    receivedData = receivedData.message;
+    console.log(receivedData);
+    input.value.value = receivedData.ip;
+    emitLocation(receivedData.location);
 }
-async function getIP() {
-    const ip = await fetch(`http://localhost:9999/.netlify/functions/getip`)
+
+function emitLocation(location) {
+    const locationString = location.replace(/ /g, "+").replace(/,/, "")
+    emit('location', locationString);
+}
+
+async function getCurrentIP() {
+    const ip = await fetch(`${local}.netlify/functions/getcurrentip`)
     let data = await ip.json()
     data = data.message
+    console.log(data);
     input.value.value = data.ip;
+    emitLocation(data.location);
 }
+
 onMounted(() => {
-    getIP();
+    getCurrentIP();
 })
 </script>
 
 <template>
     <div id="ip-search-container">
-        <form @submit.prevent="log">
+        <form @submit.prevent="find">
             <input type="text" name="ipAddress" id="ipAddress" placeholder="Search for any IP address or domain"
                 ref="input">
             <button type="submit">
