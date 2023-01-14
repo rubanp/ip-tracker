51b50158d1199e0fcc924bc65f01b6be071cb9e9
Ruban Prakash <rubanprakash@hotmail.com>
2023-01-14 19:03:28 +0000

    add google map component

---
+++ components/GoogleMap.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+const props = defineProps(['location'])
+const key = "AIzaSyCliJ2zB-Pymj0UW3xM0x6tXSE3N3c607w"
+const url =
+    ref(`https://www.google.com/maps/embed/v1/place?key=${key}&q=${props.location}`);
+console.log(props.location);
+</script>
+
+<template>
+    <div id="google-map-div">
+        <iframe :src="url" frameborder="0" loading="lazy" allowfullscreen
+            referrerpolicy="no-referrer-when-downgrade"></iframe>
+    </div>
+</template>
+
+<style scoped>
+iframe {
+    width: 600px;
+    height: 450px;
+    border: 0;
+}
+</style>
